c4ff4f1178edff59edd136213bbc1ed049564afd
Renu <100067@bq-067.bambeeq.com>
2018-07-12 17:23:53 +0530

    Corrected endpoint to use for rescope post event

--- src/main/java/org/gooru/nucleus/handlers/classes/processors/utils/AppHelper.java
+++ src/main/java/org/gooru/nucleus/handlers/classes/processors/utils/AppHelper.java
@@ -42,7 +42,7 @@ public final class AppHelper {
     
     private static void postRescopeEvent(String data, String authHeader) {
             AppHttpClient httpClient = AppHttpClient.getInstance();
-            String endPoint = httpClient.route0Endpoint();
+            String endPoint = httpClient.rescopeEndpoint();
             executeHTTPClientPost(data, authHeader, httpClient, endPoint);
     }
     
