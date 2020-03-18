f18a0b968813d4d9ebd849c35ad23ae45f73f9d2
vijayaraman.p <vijay@gooru.org>
2020-03-18 15:42:18 +0530

    changed the query to remove duplicate OA

--- src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/entities/EntityClassContentsDao.java
+++ src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/entities/EntityClassContentsDao.java
@@ -148,7 +148,7 @@ public final class EntityClassContentsDao {
       "class_id = ANY(?::uuid[]) AND dca_added_date BETWEEN ?::date AND ?::date and content_type != 'offline-activity'";
 
   private static final String SELECT_ASMT_COLL_SCHEDULED_FOR_TEACHERS_FOR_CONTENT_TYPE =
-      "class_id = ANY(?::uuid[]) AND dca_added_date BETWEEN ?::date AND ?::date and content_type = ANY(?::text[])";
+      "class_id = ANY(?::uuid[]) AND dca_added_date BETWEEN ?::date AND ?::date and content_type = ANY(?::text[]) and content_type != 'offline-activity'";
 
   private static final String SELECT_ALL_OFFLINE_SCHEDULED_FOR_TEACHERS =
       "class_id = ANY(?::uuid[]) AND (dca_added_date BETWEEN ?::date AND ?::date OR end_date BETWEEN ?::date AND ?::date) "
