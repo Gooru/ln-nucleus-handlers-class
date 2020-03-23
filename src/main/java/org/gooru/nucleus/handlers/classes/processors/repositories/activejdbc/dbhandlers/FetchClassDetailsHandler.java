4442126f7b064d82a6dc87097867b3e4842ce9ca
vijayaraman.p <vijay@gooru.org>
2020-03-23 10:38:39 +0530

    removed unused code

--- src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/dbhandlers/FetchClassDetailsHandler.java
+++ src/main/java/org/gooru/nucleus/handlers/classes/processors/repositories/activejdbc/dbhandlers/FetchClassDetailsHandler.java
@@ -1,23 +1,15 @@
 package org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.dbhandlers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 import org.gooru.nucleus.handlers.classes.constants.MessageConstants;
 import org.gooru.nucleus.handlers.classes.processors.ProcessorContext;
 import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.Utils;
 import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.dbauth.AuthorizerBuilder;
-import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.entities.AJClassMember;
 import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.entities.AJEntityClass;
-import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.entities.AJEntityCollection;
-import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.entities.AJEntityContent;
-import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.entities.AJEntityUser;
 import org.gooru.nucleus.handlers.classes.processors.repositories.activejdbc.formatter.JsonFormatterBuilder;
 import org.gooru.nucleus.handlers.classes.processors.responses.ExecutionResult;
 import org.gooru.nucleus.handlers.classes.processors.responses.MessageResponse;
 import org.gooru.nucleus.handlers.classes.processors.responses.MessageResponseFactory;
-import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.LazyList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,7 +22,6 @@ public class FetchClassDetailsHandler implements DBHandler {
   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("messages");
   private final ProcessorContext context;
   private static final String RESPONSE_BUCKET_CLASSES = "class_details";
-  private final List<String> classIdList = new ArrayList<>();
   private JsonArray classIds = null;
 
   FetchClassDetailsHandler(ProcessorContext context) {
